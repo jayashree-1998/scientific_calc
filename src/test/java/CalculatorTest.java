a4fa4f606c07898f3b64800c890c33cf57ab35d4
Jayaa Shree Laxmi Kishoore <jayashreelaxmik1998@gmail.com>
2023-03-17 15:50:33 +0530

    Adding test cases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -18,4 +18,25 @@ public class CalculatorTest {
         assertEquals("Finding factorial of a number", 120, objCalcUnderTest.factorial(5), DELTA);
         assertEquals("Finding factorial of a number", 720, objCalcUnderTest.factorial(6), DELTA);
     }
+
+    @Test
+    public void PowerTruePositive(){
+        int a = 2, b = 3;
+        double trueresult = 8.0;
+        double result = objCalcUnderTest.power(a,b);
+        assertEquals("Finding the power of a number raised to another (true positive)", trueresult, result, DELTA);
+    }
+
+    @Test
+    public void SquareRootTruePositive() {
+        assertEquals("Finding squareroot of a number (true positive)", 6, objCalcUnderTest.square_root(36), DELTA);
+        assertEquals("Finding squareroot of a number (true positive)", 7, objCalcUnderTest.square_root(49), DELTA);
+    }
+
+    @Test
+    public void SquareRootFalsePositive() {
+        assertEquals("Finding the squareroot of a number (false positive)",4, objCalcUnderTest.square_root(25), DELTA);
+        assertEquals("Finding the squareroot of a number (false positive)",5, objCalcUnderTest.square_root(36), DELTA);
+    }
+
 }
